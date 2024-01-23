e42d06c4e7774c989d3c7538ad989a72005f0816
Michael Duncan <m.duncan1488@gmail.com>
2024-01-24 10:42:23 +1300

    up

--- ESPNow_RECV_8266/ESPNow_RECV_8266.ino
+++ ESPNow_RECV_8266/ESPNow_RECV_8266.ino
@@ -1,6 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
-
+#define LED 13
 typedef struct struct_message {
   char info[32];
   float _temp;
@@ -28,6 +28,7 @@ void OnDataRecv(uint8_t* mac, uint8_t* incomingData, uint8_t len) {
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
+  pinMode(LED,OUTPUT);
   if (esp_now_init() != 0) {
     Serial.println("ERROR");
     ESP.restart();
@@ -38,5 +39,5 @@ void setup() {
 
 void loop() {
   Serial.write(myData._ledState ? "on" : "off");
-  digitalWrite(LED_BUILTIN ,myData._ledState );
+  digitalWrite(LED ,myData._ledState );
 }
--- ESPNow_send_32/ESPNow_send_32.ino
+++ ESPNow_send_32/ESPNow_send_32.ino
@@ -1,29 +1,31 @@
 #include <esp_now.h>
 #include <WiFi.h>
-
+ 
 #define LED_BUILTIN 4  //GPIO4 Flash
-
+ 
 unsigned long previousMillis = 0;  // will store last time LED was updated
-
+ 
 const long interval = 1000;                                           // interval at which to blink
 uint8_t broadcastAddress[] = { 0x98, 0xCD, 0xAC, 0x31, 0x59, 0xBC };  // Mac address for reciver board
-
+ 
+bool ledState = 0;
+ 
 typedef struct struct_message {  // struct to hold variables to send
   char info[32];
   float _temp;
   bool _ledState;
 } struct_message;
-
+ 
 struct_message myData;
 esp_now_peer_info_t peerInfo;  // variable to hold mac/channel info to connect
-
-
+ 
+ 
 // Serial print after each packet sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("Last Data Status:  ");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Fail");
 }
-
+ 
 void setup() {
   Serial.begin(115200);       // start serial @115200
   WiFi.mode(WIFI_STA);        // set wifi to station mode
@@ -33,11 +35,11 @@ void setup() {
     return;         // exit
   }
   esp_now_register_send_cb(OnDataSent);  //
-
+ 
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
-
+ 
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Failed to connect to Peer");
     ESP.restart();
@@ -46,43 +48,26 @@ void setup() {
 }
 void loop() {
   unsigned long currentMillis = millis();
-  int ledState = 0;
-  bool _ledState = true;
-
-
-  Serial.print("currentMillis : ");
-  Serial.println(currentMillis);
-
-
+ 
+ // Serial.print("currentMillis : ");
+  //Serial.println(currentMillis);
+ 
   // if elapsed time is greater the interval
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;  // reset timer to 0
-
-    if (ledState == LOW) {  // switch led on
-      ledState = 0;
-      Serial.println("FLASH ON");
-      Serial.println(myData._ledState);
-
+    digitalWrite(LED_BUILTIN, ledState = !ledState);  // toggle led on/off
+      Serial.println(ledState ? "Led is ON" : "Led is OFF");
+    
+    // data to be sent
+    strcpy(myData.info, "esp32");         // display board name
+    myData._ledState = ledState;      // ledstate
+    myData._temp = random(1.00, 100.00);  // temps
+ 
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
+    if (result == ESP_OK) {
+      Serial.println("SENT WITH SUCCESS");
     } else {
-      // switch led off
-      ledState = 1;
-      Serial.println("FLASH OFF");
+      Serial.println("SEND FAILED");
     }
-
-    digitalWrite(LED_BUILTIN, ledState);  // toggle led on/off
-    _ledState = ledState;
-  }
-  // data to be sent
-  strcpy(myData.info, "esp32");         // display board name
-  myData._ledState = _ledState;      // ledstate
-  myData._temp = random(1.00, 100.00);  // temps
-
-  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
-  if (result == ESP_OK) {
-    Serial.println("SENT WITH SUCCESS");
-    delay(1000);
-  } else {
-    Serial.println("SEND FAILED");
-    delay(2000);
   }
 }
